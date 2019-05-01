2a8b74d15cf84e0f8b79f3f332fc3f9e0965507f
jonathanssaldanha <jonathanssaldanha@gmail.com>
2019-05-01 00:09:45 -0300

    Programa Ler e Ecrever em Java v.1

---
+++ LeituraCidades/src/Cidade.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 0101765
+ */
+public class Cidade {
+    private String nome;
+    private Integer x;
+    private Integer y;
+
+    @Override
+    public String toString() {
+        return getNome() + " X:" + getX() + " Y:" + getY();
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the x
+     */
+    public Integer getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y
+     */
+    public Integer getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(Integer y) {
+        this.y = y;
+    }
+    
+    
+}
